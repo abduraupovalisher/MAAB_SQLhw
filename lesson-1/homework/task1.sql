bac283be24bd02fb4cdec694645a926106c5f59c
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2025-06-03 00:26:57 +0600

    Update task1.sql

--- lesson-1/homework/task1.sql
+++ lesson-1/homework/task1.sql
@@ -1,1 +1,11 @@
+-- Create table without NOT NULL
+CREATE TABLE student (
+    id INT,
+    name VARCHAR(100),
+    age INT
+);
+
+-- Add NOT NULL constraint to id
+ALTER TABLE student
+MODIFY id INT NOT NULL;
 
