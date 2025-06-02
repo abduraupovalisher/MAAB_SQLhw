44676b2af3d815e83f800961a7537d3d61c19cbb
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2025-06-03 00:35:18 +0600

    Update task6.sql

--- lesson-1/homework/task6.sql
+++ lesson-1/homework/task6.sql
@@ -1,1 +1,15 @@
+-- Create customer table with DEFAULT
+CREATE TABLE customer (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(100) DEFAULT 'Unknown'
+);
+
+-- Drop DEFAULT
+ALTER TABLE customer
+ALTER city DROP DEFAULT;
+
+-- Re-add DEFAULT
+ALTER TABLE customer
+ALTER city SET DEFAULT 'Unknown';
 
