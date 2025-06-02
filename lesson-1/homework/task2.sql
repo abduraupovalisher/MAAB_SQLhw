08bac65ab35dc7d5f29b540b8eeb1d76bea322f0
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2025-06-03 00:32:13 +0600

    Update task2.sql

--- lesson-1/homework/task2.sql
+++ lesson-1/homework/task2.sql
@@ -1,1 +1,19 @@
+-- Create table with UNIQUE constraint
+CREATE TABLE product (
+    product_id INT UNIQUE,
+    product_name VARCHAR(100),
+    price DECIMAL(10, 2)
+);
+
+-- Drop UNIQUE constraint (MySQL-style: constraint must be named or implied)
+ALTER TABLE product
+DROP INDEX product_id;
+
+-- Add UNIQUE constraint again
+ALTER TABLE product
+ADD UNIQUE (product_id);
+
+-- Add UNIQUE constraint on combination of product_id and product_name
+ALTER TABLE product
+ADD CONSTRAINT unique_product_combination UNIQUE (product_id, product_name);
 
