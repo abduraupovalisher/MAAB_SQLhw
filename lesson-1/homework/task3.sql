77ef24701cb2f0c3427a6352a8ba1be6231ac903
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2025-07-09 11:27:46 +0600

    Update task3.sql

--- lesson-1/homework/task3.sql
+++ lesson-1/homework/task3.sql
@@ -1,15 +1,12 @@
--- Create table with PRIMARY KEY
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
     customer_name VARCHAR(100),
     order_date DATE
 );
 
--- Drop PRIMARY KEY
 ALTER TABLE orders
 DROP PRIMARY KEY;
 
--- Add PRIMARY KEY again
 ALTER TABLE orders
 ADD PRIMARY KEY (order_id);
 
